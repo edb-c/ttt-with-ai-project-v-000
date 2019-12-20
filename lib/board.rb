8f3826d3825cae90d526b54ef1cbb7917b411d0d
Ethel Cofresi <cofresi.ethel@gmail.com>
2019-12-20 20:38:00 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -11,6 +11,8 @@ class Board
   end
   
   def display
+    puts ""
+    puts "------------------------------------------"
     puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
     puts "-----------"
     puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
